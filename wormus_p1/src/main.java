942b119cb6b41de3df6af05329e61cabb4a524bd
Annie Wormus <annie@wormus.com>
2020-09-12 18:16:44 -0400

    adds user input

--- wormus_p1/src/main.java
+++ wormus_p1/src/main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String args[]){
-        System.out.print("what up happy world?");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int num = scan.nextInt();
+        System.out.println("Your number is "+ num);
+
     }
 }
