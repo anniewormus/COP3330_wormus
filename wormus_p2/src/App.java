3c5258ca4f844632976e7d051329d1b92cefede7
Annie Wormus <annie@wormus.com>
2020-09-25 14:24:33 -0400

    set up methods and classes according to given main method

--- wormus_p2/src/App.java
+++ wormus_p2/src/App.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class App {
+    //DO NOT TOUGH OR YOU WILL BE CRUCIFIED
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
+
+    private static boolean moreInput() {
+        return true;
+    }
+
+    private static double getUserWeight() {
+        return 0;
+    }
+
+    private static double getUserHeight() {
+        return 0;
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi){
+
+    }
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData){
+
+    }
 }
--- wormus_p2/src/BodyMassIndex.java
+++ wormus_p2/src/BodyMassIndex.java
@@ -1,2 +1,24 @@
 public class BodyMassIndex {
+    private double height;
+    private double weight;
+
+    //getters
+    public double getUserHeight(){
+        return height;
+    }
+    public double getUserWeight(){
+        return weight;
+    }
+    //setters
+    public void setUserHeight(double newHeight) {
+        this.height = newHeight;
+    }
+    public void setUserWeight(double newWeight){
+        this.weight = newWeight;
+    }
+    //counstructor
+    public BodyMassIndex(double height, double weight){
+
+    }
+
 }
