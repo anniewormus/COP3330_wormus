90cb91ef3bb135ef4bd79170e27c1bd307094e7f
Annie Wormus <annie@wormus.com>
2020-09-28 19:05:04 -0400

    finished program, rough draft

--- wormus_p2/src/App.java
+++ wormus_p2/src/App.java
@@ -1,7 +1,13 @@
+/* Andrea Wormus
+    This program calculates BMI and BMI category based on user input of weight and height values.
+ */
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
-    //DO NOT TOUGH OR YOU SO HELP ME
+
+    //NO TOUCHY
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
 
@@ -17,23 +23,56 @@ public class App {
 
         displayBmiStatistics(bmiData);
     }
+    public static Scanner in = new Scanner(System.in);
 
     private static boolean moreInput() {
-        return true;
+        System.out.println("Would you like to calculate a BMI? (Y/N) ");
+        String cont = in.nextLine();
+        if(cont.equals("Y")) return true;
+        else if(cont.equals("N")) {
+            System.out.println("Goodbye!");
+            return false;
+        }else{
+            System.out.println("Sorry I didn't catch that. Goodbye!");
+            return false;
+        }
     }
 
     private static double getUserWeight() {
-        return 0;
+            System.out.println("Enter weight (in pounds): ");
+            double weight = in.nextDouble();
+            in.nextLine();
+            while(weight < 0) {
+                System.out.print("Wow what's your workout routine? Try again.");
+                weight = in.nextDouble();
+            }
+        return weight;
     }
 
     private static double getUserHeight() {
-        return 0;
+        System.out.println("Enter height (in inches): ");
+        double height = in.nextDouble();
+        in.nextLine();
+        while(height < 0) {
+            System.out.print("How's the weather down there? Try again.");
+            height = in.nextDouble();
+        }
+        return height;
     }
 
     public static void displayBmiInfo(BodyMassIndex bmi){
+        System.out.println("Your BMI is: " + bmi.getBMI() +
+                            "\nYou are " + bmi.getCategories());
 
     }
-    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData){
-
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+        double add = 0;
+        double count = bmiData.size();
+        for (int i = 0; i < count; i++) {
+            BodyMassIndex current = bmiData.get(i);
+            add += current.getBMI();
+        }
+        double avgBMI = add / count;
+        System.out.println("Average BMI is " + avgBMI);
     }
 }
--- wormus_p2/src/BodyMassIndex.java
+++ wormus_p2/src/BodyMassIndex.java
@@ -1,11 +1,28 @@
 public class BodyMassIndex {
     public double h;
     public double w;
+    public double BMI;
 
     //counstructor
     public BodyMassIndex(double height, double weight){
         h = height;
-        w = weight
+        w = weight;
     }
-
+    public double getBMI(){
+        double h2 = h * h;
+        BMI = 703 * w / h2;
+        return BMI;
+    }
+    public String getCategories(){
+        if(BMI < 18.50){
+            return "underweight";
+        }else if (BMI >= 18.50 && BMI < 25.00){
+            return "normal weight";
+        }else if (BMI >= 25.00 && BMI < 30.00){
+            return "overweight";
+        }else if (BMI >= 30.00){
+            return "obese";
+        }
+        return "";
+    }
 }
