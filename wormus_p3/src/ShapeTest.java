3e3c74b19b8b3a5fa99e19e111ba2b3fc5b62544
Annie Wormus <annie@wormus.com>
2020-10-12 18:43:51 -0400

    initial commit

---
+++ wormus_p3/src/ShapeTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapeTest {
+    @Test
+    public void testSquareName(){
+        Shape shape = new Square(10);
+        assertEquals("square", shape.getName());
+    }
+}
